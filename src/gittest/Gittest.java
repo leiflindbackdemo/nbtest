5de72c190e8ba22eb2c3b2a7327123757b88d7e0
leif <>
2017-03-27 17:00:45 +0200

    some changes

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gittest;
 
 /**
- *
+ * Adding a new comment that will appear also on github
  * @author leif
  */
 public class Gittest {
